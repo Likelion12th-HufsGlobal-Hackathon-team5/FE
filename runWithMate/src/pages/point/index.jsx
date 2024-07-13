3ae6a42af91628111cd6bfaba3b639a2538541cc
jihee <esder1310@naver.com>
2024-07-13 17:55:34 +0900

    docs : point

---
+++ runWithMate/src/pages/point/index.jsx
@@ -0,0 +1,16 @@
+import styled from '@emotion/styled'
+
+const Container=styled.div``;
+
+function Point(){
+
+    return(
+        <>
+            <Container>
+                point 페이지
+            </Container>
+        </>
+    )
+}
+
+export default Point;
