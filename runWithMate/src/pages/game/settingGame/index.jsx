483d4bbe4f85eaba243119fbbeb3622045836a8b
jihee <esder1310@naver.com>
2024-07-13 17:54:27 +0900

    docs : settingGame

---
+++ runWithMate/src/pages/game/settingGame/index.jsx
@@ -0,0 +1,16 @@
+import styled from '@emotion/styled'
+
+const Container=styled.div``;
+
+function SettingGame(){
+
+    return(
+        <>
+            <Container>
+            SettingGame 페이지
+            </Container>
+        </>
+    )
+}
+
+export default SettingGame;
