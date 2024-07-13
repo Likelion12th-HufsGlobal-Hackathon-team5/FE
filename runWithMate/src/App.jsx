f07b613706becec525c1c190e776e10801dc9d1c
jihee <esder1310@naver.com>
2024-07-13 17:57:37 +0900

    docs : src

---
+++ runWithMate/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import { Route, BrowserRouter as Router, Routes} from 'react-router-dom';
+import routes from './routes';
+import Mobile from './styles/mobile';
+
+function App() {
+  const elements = routes.map((item, index) => (
+    <Route key={index} path={item.path} element={item.element} />
+  ));
+  return (
+    <>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Mobile />}>
+          {elements}
+        </Route>
+      </Routes>
+    </Router>
+    </>
+  );
+}
+
+export default App;
---
+++ runWithMate/src/main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+
+import { createRoot } from 'react-dom/client'
+
+
+const rootElement=document.getElementById('root');
+createRoot(rootElement).render(
+  <>
+    <App/>
+  </>
+);
