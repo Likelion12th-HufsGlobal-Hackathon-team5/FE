e00d12ef0ef50842882c25f5d4f2d31d200ef785
jihee <esder1310@naver.com>
2024-07-13 17:54:11 +0900

    docs : gameResult

---
+++ runWithMate/src/pages/game/gameResult/index.jsx
@@ -0,0 +1,16 @@
+import styled from '@emotion/styled'
+
+const Container=styled.div``;
+
+function GameResult(){
+
+    return(
+        <>
+            <Container>
+                gameResult 페이지
+            </Container>
+        </>
+    )
+}
+
+export default GameResult;
