98267bac4463a0414d9ca0618150fe962c97f513
jihee <esder1310@naver.com>
2024-07-13 17:55:18 +0900

    docs : mypage

---
+++ runWithMate/src/pages/mypage/index.jsx
@@ -0,0 +1,15 @@
+import styled from '@emotion/styled'
+
+const Container=styled.div``;
+function Mypage(){
+
+    return(
+        <>
+            <Container>
+                mypage 페이지
+            </Container>
+        </>
+    )
+}
+
+export default Mypage;
