bdc0ca9891e187e403c8aa23fa7cec0f5e31cd58
jihee <esder1310@naver.com>
2024-07-14 21:47:21 +0900

    docs : main.jsx
    globalstyle 적용

--- runWithMate/src/main.jsx
+++ runWithMate/src/main.jsx
@@ -2,9 +2,13 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 
+import { Global } from '@emotion/react';
+import globalStyles from './styles/globalstyles.jsx';
+
 const rootElement = document.getElementById('root');
 createRoot(rootElement).render(
   <React.StrictMode>
+    <Global styles={globalStyles} />
     <App />
   </React.StrictMode>
 );
