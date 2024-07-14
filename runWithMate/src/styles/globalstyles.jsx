3c9aad1fb72f7cdfebdd36aef5b5de44e66432a2
jihee <esder1310@naver.com>
2024-07-14 21:47:01 +0900

    docs : globalstyles.jsx
    globalstyle 설정

---
+++ runWithMate/src/styles/globalstyles.jsx
@@ -0,0 +1,15 @@
+import { css } from '@emotion/react';
+
+const globalStyles = css`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  body {
+    font-family: 'Arial', sans-serif;
+    background-color: #f0f0f0;
+  }
+`;
+
+export default globalStyles;
