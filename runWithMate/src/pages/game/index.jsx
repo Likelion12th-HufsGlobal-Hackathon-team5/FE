4ab6ae1d3648504c074f5d22f7c27a755e872eca
jihee <esder1310@naver.com>
2024-07-14 21:46:39 +0900

    docs : game
    websocket, kakaomap 설정 주석처리

--- runWithMate/src/pages/game/index.jsx
+++ runWithMate/src/pages/game/index.jsx
@@ -4,52 +4,51 @@ import styled from '@emotion/styled'
 const Container=styled.div``;
 
 function Game(){
-    const [position, setPosition]=useState([]);
-    const mapRef=useRef(null);
-    const map=useRef(null);
+    // const [position, setPosition]=useState([]);
+    // const mapRef=useRef(null);
+    // const map=useRef(null);
 
-    useEffect(()=>{
-        //websocket 초기화
-        const ws = new Websocket('ws URL')
+    // useEffect(()=>{
+    //     //websocket 초기화
+    //     const ws = new Websocket('ws URL')
 
-        ws.onmessage=(message)=>{
-            const data=JSON.parse(message.data);
-            setPosition((prevPosition)=>[...prevPositions,{ lat:data.lat, lng: data.lng}]);
-        };
+    //     ws.onmessage=(message)=>{
+    //         const data=JSON.parse(message.data);
+    //         setPosition((prevPosition)=>[...prevPositions,{ lat:data.lat, lng: data.lng}]);
+    //     };
 
-        //websocket종료시
-        return () => {
-            ws.close();
-        };
-    },[]);
+    //     //websocket종료시
+    //     return () => {
+    //         ws.close();
+    //     };
+    // },[]);
 
-    useEffect(()=>{
-        //kakao map 초기화
-        if (mapRef.current){
-            map.current=new window.kakao.maps.Map(mapRef.current,{
-                center : new window.kakao.maps.LatLng(33.450701, 126.570667),
-                level:3,
-            });
-        }
-    },[]);
+    // useEffect(()=>{
+    //     //kakao map 초기화
+    //     if (mapRef.current){
+    //         map.current=new window.kakao.maps.Map(mapRef.current,{
+    //             center : new window.kakao.maps.LatLng(33.450701, 126.570667),
+    //             level:3,
+    //         });
+    //     }
+    // },[]);
 
-    useEffect(()=>{
-        if (map.current){
-            position.forEach((position)=>{
-                const marker = new window.kakao.maps.Marker({
-                    position:new window.kakao.maps.LatLng(position.lat,position.lng),
-                    map:map.current,
-                });
-            });
-        }
-    },[positions]);
+    // useEffect(()=>{
+    //     if (map.current){
+    //         position.forEach((position)=>{
+    //             const marker = new window.kakao.maps.Marker({
+    //                 position:new window.kakao.maps.LatLng(position.lat,position.lng),
+    //                 map:map.current,
+    //             });
+    //         });
+    //     }
+    // },[positions]);
 
     return(
         <>
             <Container>
                 game 페이지
-
-                <div ref={mapRef} style={{width:'100%', height:"100vh"}} />
+                {/* <div ref={mapRef} style={{width:'100%', height:"100vh"}} /> */}
             </Container>
         </>
     )
