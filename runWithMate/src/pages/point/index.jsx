35e0ba70f67126ec93018dd08830e4a468086ae8
norangsuji <sujihong93@gmail.com>
2024-07-21 17:55:15 +0900

    point page index.jsx 작성

--- runWithMate/src/pages/point/index.jsx
+++ runWithMate/src/pages/point/index.jsx
@@ -1,16 +1,32 @@
 import styled from '@emotion/styled'
-
-const Container=styled.div``;
+import Header from '../../components/Header';
+import MyPointBox from '../../components/point/MyPointBox';
+import AttendanceBtn from '../../components/point/AttendanceBtn';
+import PointMarket from '../../components/point/PointMarket';
+import MyWallet from '../../components/point/MyWallet';
 
 function Point(){
 
     return(
         <>
-            <Container>
-                point 페이지
-            </Container>
+        <Header></Header>
+        <Container> 
+                <MyPointBox></MyPointBox>
+                <AttendanceBtn></AttendanceBtn>
+                <PointMarket></PointMarket>
+                <MyWallet></MyWallet>
+        </Container>
+            
         </>
-    )
+    )   
 }
 
 export default Point;
+
+
+const Container=styled.div`
+    margin: 4vh;
+    display: flex;
+    flex-direction: column;
+    gap: 3vh;
+`;
