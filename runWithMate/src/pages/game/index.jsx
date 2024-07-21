992d7059d988d0a279991b37f05c6b6624f3f26f
jihee <esder1310@naver.com>
2024-07-21 16:58:01 +0900

    remove : game.jsx
    안쓰는 js code 삭제

--- runWithMate/src/pages/game/index.jsx
+++ runWithMate/src/pages/game/index.jsx
@@ -171,47 +171,6 @@ const GameRankingInfoHeader=styled.div`
 `;
 
 function Game(){
-    // const [position, setPosition]=useState([]);
-    // const mapRef=useRef(null);
-    // const map=useRef(null);
-
-    // useEffect(()=>{
-    //     //websocket 초기화
-    //     const ws = new Websocket('ws URL')
-
-    //     ws.onmessage=(message)=>{
-    //         const data=JSON.parse(message.data);
-    //         setPosition((prevPosition)=>[...prevPositions,{ lat:data.lat, lng: data.lng}]);
-    //     };
-
-    //     //websocket종료시
-    //     return () => {
-    //         ws.close();
-    //     };
-    // },[]);
-
-    // useEffect(()=>{
-    //     //kakao map 초기화
-    //     if (mapRef.current){
-    //         map.current=new window.kakao.maps.Map(mapRef.current,{
-    //             center : new window.kakao.maps.LatLng(33.450701, 126.570667),
-    //             level:3,
-    //         });
-    //     }
-    // },[]);
-
-    // useEffect(()=>{
-    //     if (map.current){
-    //         position.forEach((position)=>{
-    //             const marker = new window.kakao.maps.Marker({
-    //                 position:new window.kakao.maps.LatLng(position.lat,position.lng),
-    //                 map:map.current,
-    //             });
-    //         });
-    //     }
-    // },[positions]);
-
-
     //이하 모달창의 상태관리
 
     const [isGiveupOpen, setIsGiveupOpen] = useState(false);
