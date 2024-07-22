d5d54758080a7486aefa8c7e906ebb9e4b2ff3a0
jihee <esder1310@naver.com>
2024-07-23 01:30:42 +0900

    feat : Game.jsx
    지정좌표에 2종류 마커 띄우기 성공

--- runWithMate/src/components/game/Map.jsx
+++ runWithMate/src/components/game/Map.jsx
@@ -1,7 +1,9 @@
 import React, { useEffect } from 'react';
 import { Global, css } from '@emotion/react';
 import styled from '@emotion/styled';
+
 import PointMarkerImg from '../../assets/images/pointMarker.png';
+import DopamineMarkerImg from '../../assets/images/dopamineMarker.png';
 
 const Container = styled.div`
   display: flex;
@@ -46,18 +48,79 @@ function Map() {
 
       const map = new window.kakao.maps.Map(mapContainer, mapOption);
 
-      const imageSrc = PointMarkerImg;  
-      const imageSize = new window.kakao.maps.Size(64/2, 69/2); 
-      // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
-      const imageOption = { offset: new window.kakao.maps.Point(27, 69) }; 
+      const PointPositions = [
+        {
+          title: '한국외국어대학교 글로벌캠퍼스 정문',
+          latlng: new window.kakao.maps.LatLng(37.3357, 127.2558)
+        },
+        {
+          title: '한국외대 글캠 기숙사',
+          latlng: new window.kakao.maps.LatLng(37.3353, 127.2626)
+        },
+        {
+          title: '한국외대 백년관',
+          latlng: new window.kakao.maps.LatLng(37.3376, 127.2658 )
+        },
+        {
+          title: '한국외대 공학관',
+          latlng: new window.kakao.maps.LatLng(37.3376, 127.2678)
+        },
+        {
+          title: '한국외대 학생회관',
+          latlng: new window.kakao.maps.LatLng(37.3372, 127.2698)
+        }
+      ];
 
-      const markerImage = new window.kakao.maps.MarkerImage(imageSrc, imageSize, imageOption);
-      const markerPosition = new window.kakao.maps.LatLng(lat, lng);
-      const marker = new window.kakao.maps.Marker({
-        position: markerPosition,
-        image: markerImage, // 마커이미지 설정 
-      });
-      marker.setMap(map);
+      const DopaminePositions = [
+        {
+          title: '도파민01',
+          latlng: new window.kakao.maps.LatLng(37.3355, 127.2578)
+        },
+        {
+          title: '도파민02',
+          latlng: new window.kakao.maps.LatLng(37.3365, 127.2644)
+        },
+        {
+          title: '도파민03',
+          latlng: new window.kakao.maps.LatLng(37.3368, 127.267 )
+        },
+        {
+          title: '도파민04',
+          latlng: new window.kakao.maps.LatLng(37.3377, 127.2685)
+        }
+      ];
+
+      const pointImageSrc = PointMarkerImg;
+      const pointImageSize = new window.kakao.maps.Size(32, 40.61);
+      const pointImageOption = { offset: new window.kakao.maps.Point(16, 34.5) };
+
+      const pointMarkerImage = new window.kakao.maps.MarkerImage(pointImageSrc, pointImageSize, pointImageOption);
+
+      for (let i = 0; i < PointPositions.length; i++) {
+        const pointMarker = new window.kakao.maps.Marker({
+          map: map,
+          position: PointPositions[i].latlng,
+          title: PointPositions[i].title,
+          image: pointMarkerImage
+        });
+        pointMarker.setMap(map);
+      }
+
+      const dopaminImageSrc = DopamineMarkerImg;
+      const dopaminImageSize = new window.kakao.maps.Size(32, 40.61);
+      const dopaminImageOption = { offset: new window.kakao.maps.Point(16, 34.5) };
+
+      const dopaminMarkerImage = new window.kakao.maps.MarkerImage(dopaminImageSrc, dopaminImageSize, dopaminImageOption);
+
+      for (let i = 0; i < DopaminePositions.length; i++) {
+        const dopaminMarker = new window.kakao.maps.Marker({
+          map: map,
+          position: DopaminePositions[i].latlng,
+          title: DopaminePositions[i].title,
+          image: dopaminMarkerImage
+        });
+        dopaminMarker.setMap(map);
+      }
     };
 
     const showError = (error) => {
