83f3151946b95137774ba73f7b20a4cfed67384e
norangsuji <sujihong93@gmail.com>
2024-07-29 00:01:43 +0900

    design PointShop

---
+++ runWithMate/src/components/point/marketBtn.jsx
@@ -0,0 +1,74 @@
+import React from "react";
+import styled from "@emotion/styled";
+import { BsCapsulePill } from "react-icons/bs";
+import { CgGym } from "react-icons/cg";
+
+function MarketBtn() {
+    const StyledBsCapsulePill = styled(BsCapsulePill)`
+        font-size: 30px;
+    `;
+    const StyledCgGym = styled(CgGym)`
+        font-size: 40px;
+        margin: -0.52vh -0.3vh;
+    `;
+  return (
+    <Container>
+      <ButtonBox>
+        <StyledBsCapsulePill/>
+        <StyledParagraph fontWeight="700" fontSize="16px">건강 기능 식품</StyledParagraph> 구매하러 가기
+      </ButtonBox>
+      <ButtonBox>
+        <StyledCgGym/>
+        <StyledParagraph fontWeight="700" fontSize="16px">헬스 용품</StyledParagraph> 구매하러 가기
+      </ButtonBox>  
+    </Container>
+  );
+}
+
+export default MarketBtn;
+
+const Container = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: column;
+  gap: 1vh;
+  margin-top: 1vh;
+  justify-content: center;
+`;
+
+const ButtonBox = styled.button`
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: row;
+  justify-content: baseline;
+  align-items: center;
+  font-weight: 300;
+  font-size: 16px;
+  gap: 1vh;
+  color: #ffffff;
+  background-color: #42e19d;
+  border: #42e19d solid 3px;
+  padding: 2vh;
+  border-radius: 2vh;
+  cursor: pointer;
+  transition: background-color 0.3s, color 0.3s, border 0.3s, box-shadow 0.3s;
+
+  &:hover {
+    background-color: #1DCF82;
+    color: #ffffff;
+    border: #1DCF82 solid 3px;
+  }
+
+  &:active {
+    background-color: #ffffff;
+    color: #23dc8c;
+    border: #ffffff solid 3px;
+    box-shadow: 0 0 5px rgba(0, 0, 0, 0.5);
+  }
+`;
+
+const StyledParagraph = styled.p`
+  font-weight: ${(props) => props.fontWeight || "normal"};
+  font-size: ${(props) => props.fontSize || "16px"};
+  margin: 0; // 기본 p 태그의 마진을 제거
+`;
