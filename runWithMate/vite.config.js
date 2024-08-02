1245a46d62beca5aee10882c2ff98c773909505e
jihee <esder1310@naver.com>
2024-08-03 00:59:40 +0900

    feat : PWA settings
    PWA setting 완료

--- runWithMate/vite.config.js
+++ runWithMate/vite.config.js
@@ -7,12 +7,34 @@
 // })
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/auth/kakao': 'http://localhost:5173',
-    },
-  },
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg', 'robots.txt', 'apple-touch-icon.png'],
+      manifest: {
+        name: 'My PWA App',
+        short_name: 'PWA',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#000000',
+        icons: [
+          {
+            src: '/RWM_PWA_icon.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/RWM_PWA_icon.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ]
 });
