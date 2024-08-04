5bf225ef633fafe0cdafdba71876e8e317b5ebee
norangsuji <sujihong93@gmail.com>
2024-08-04 21:47:47 +0900

    DesignGameResult: ui 수정

--- runWithMate/src/pages/game/gameResult/index.jsx
+++ runWithMate/src/pages/game/gameResult/index.jsx
@@ -31,6 +31,7 @@ const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  justify-content: center;
   flex-wrap: wrap;
   gap: 3vh;
   box-sizing: border-box;
