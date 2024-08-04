c64cce565760a9e5c0b813aaa8000f42dadc5438
leestana01 <leestana01@naver.com>
2024-08-04 19:05:38 +0900

    서버 주소 변경

--- runWithMate/src/server/serverInfo.js
+++ runWithMate/src/server/serverInfo.js
@@ -1,3 +1,7 @@
-const defaultAddress = "localhost:8080";
-export const serverIp = `http://${defaultAddress}`;
-export const wsIp = `ws://${defaultAddress}`;
+// 로컬 개발이면 false, 서버 배포시 true 로 설정해주세요!
+export const isProduction = true;
+
+// 하단 수정 금지
+const defaultAddress = isProduction ? "api.runwithmate.klr.kr" : "localhost:8080";
+export const serverIp = (isProduction ? "https" : "http") +`://${defaultAddress}`;
+export const wsIp = (isProduction ? "wss" : "ws") + `://${defaultAddress}`;
