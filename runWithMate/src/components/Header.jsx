87d70aa9aff576fed54380a2ade910b4f2c23a25
jihee <esder1310@naver.com>
2024-08-06 03:47:08 +0900

    feat : header.jsx
    header 아이콘 변경, localStorage 기능 변경

--- runWithMate/src/components/Header.jsx
+++ runWithMate/src/components/Header.jsx
@@ -5,7 +5,7 @@ import styled from '@emotion/styled';
 import UseStomp from '../hooks/useStomp';
 
 import Icon from '/img/NoBgIcon.png';
-import { BsHouseDoorFill } from 'react-icons/bs';
+import { BsHouseDoorFill,BsPersonFill } from 'react-icons/bs';
 
 const Container=styled.div`
     display: flex;
@@ -17,15 +17,49 @@ const Container=styled.div`
     border-bottom: 3px solid #2E2929;
 
     .Home{
-        margin-right: 3vh;
+        /* margin-right: 3vh; */
+        margin-left: 5vh;
+        width: 24px;
+        height: auto;
 
+        color: #2E2929;
+        /* background-color: red; */
+    }
+
+    .myPage{
+        margin-right: 3vh;
         width: 24px;
         height: auto;
 
         color: #2E2929;
     }
 `;
-const Background=styled.div`
+// const Background=styled.div`
+//     display: flex;
+//     flex-direction: row;
+//     align-items: center;
+//     justify-content: left;
+
+//     padding: 1.5vh;
+
+//     flex-wrap: wrap;
+//     gap:1.2vw;
+
+//     color: white;
+//     font-weight: 700;
+
+//     width: 60%;
+//     background-color: #2E2929;
+//     clip-path: polygon(0 0, 60% 0, 70% 100%, 0% 100%);
+  
+//     img{
+//         margin-left: 1vh;
+
+//         width: 4.5vh;
+//         height: auto;
+//     }
+// `;
+const Background=styled(Link)`
     display: flex;
     flex-direction: row;
     align-items: center;
@@ -82,17 +116,28 @@ function Header(){
     const prevIconRef = useRef();
     const isInitialMount = useRef(true);
 
-    const handleIcon = () => {
+    const [isLogin,setIsLogin]=useState(false);
+
+    
+    const handleLoginFalse = () => {
+        if(!localStorage.getItem('userId')){
+            alert('먼저 로그인을 하셔야합니다!');
+            navigate('/login');
+        }
         setIcon(!icon);
+        setIsLogin(prevState => !prevState);
     };
-//
+
+    const handleLoginTrue=()=>{
+        setIcon(!icon);
+    }
     useEffect(() => {
         if (prevIconRef.current !== icon&& location.pathname === '/main' ) {
             console.log('header click event - disconnect');
             disconnect();
             if(localStorage.getItem("roomId")){
-                localStorage.clear("look");
-                localStorage.clear("roomId");
+                localStorage.removeItem("look");
+                localStorage.removeItem("roomId");
                 // localStorage.clear("useSTomp-getMessage");
             }
         }
@@ -101,19 +146,49 @@ function Header(){
     return(
         <>
             <Container>
-                <Background>
-                    <img src={Icon} /> 
-                    <Text>
-                        <p>
-                            Run
-                            <span className='with'>with</span>
-                        </p> 
-                        <br />
-                        Mate<br />
-                    </Text>
-                </Background>
-                <StyledLink to={'/main'} onClick={handleIcon}>
+                {/* <StyledLink to={'/main'}>
+                    <Background onClick={handleIcon}>
+                        <img src={Icon} /> 
+                        <Text>
+                            <p>
+                                Run
+                                <span className='with'>with</span>
+                            </p> 
+                            <br />
+                            Mate<br />
+                        </Text>
+                    </Background>
+                </StyledLink> */}
+                {!isLogin?(
+                    <Background to={'/main'} onClick={handleLoginTrue}>
+                            <img src={Icon} /> 
+                            <Text>
+                                <p>
+                                    Run
+                                    <span className='with'>with</span>
+                                </p> 
+                                <br />
+                                Mate<br />
+                            </Text>
+                        </Background>
+                ):(
+                    <Background to={'/login'} onClick={handleLoginFalse}>
+                            <img src={Icon} /> 
+                            <Text>
+                                <p>
+                                    Run
+                                    <span className='with'>with</span>
+                                </p> 
+                                <br />
+                                Mate<br />
+                            </Text>
+                        </Background>
+                )}
+                {/* <StyledLink to={'/main'} onClick={handleIcon}>
                     <BsHouseDoorFill className='Home'/>
+                </StyledLink> */}
+                <StyledLink to={'/mypage'}>
+                    <BsPersonFill className='myPage'/>
                 </StyledLink>
             </Container>
         </>
