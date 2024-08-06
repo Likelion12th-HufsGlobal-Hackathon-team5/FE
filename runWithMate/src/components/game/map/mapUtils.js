b33cc82013f23a87ae23c96d82b5bc8ab008b86f
leestana01 <leestana01@naver.com>
2024-08-06 15:38:51 +0900

    feat: 게임 내부 구현 완료

--- runWithMate/src/components/game/map/mapUtils.js
+++ runWithMate/src/components/game/map/mapUtils.js
@@ -25,28 +25,30 @@ export const initializeMap = (setMap, setCurrentLocationMarker, setLoading) => {
       const mapContainer = document.getElementById('map');
       const mapOption = {
         center: new window.kakao.maps.LatLng(latitude, longitude),
-        level: 3,
+        level: 4,
       };
-
+      console.log('지도 초기화 시작');
       const newMap = new window.kakao.maps.Map(mapContainer, mapOption);
       setMap(newMap);
 
+      console.log('현재 위치 마커 생성');
       const currentMarker = new window.kakao.maps.Marker({
         map: newMap,
         // position: new window.kakao.maps.LatLng(latitude, longitude),
-        title: '현재 위치',
+        title: localStorage.getItem('userId'),
       });
       setCurrentLocationMarker(currentMarker);
 
       // addMarkers(newMap, mockPointBoxes, '/img/pp.png');
       // addMarkers(newMap, mockDopamineBoxes, '/img/dp.png');
 
-      // setLoading(false);
+      setLoading(false);
+      console.log('지도 초기화 완료');
     }, showError, { enableHighAccuracy: true, timeout: 30000, maximumAge: 0 });
   }
 };
 
-export const addMarkers = (map, markerData, markerImageSrc) => {
+export const addMarkers = (map, setMarkers, markerData, markerImageSrc) => {
   const markerImage = new window.kakao.maps.MarkerImage(
     markerImageSrc,
     new window.kakao.maps.Size(32.8, 48),
@@ -54,11 +56,12 @@ export const addMarkers = (map, markerData, markerImageSrc) => {
   );
 
   markerData.forEach(position => {
-    new window.kakao.maps.Marker({
+    const newMarker = new window.kakao.maps.Marker({
       map: map,
       position: position.latlng,
       title: position.title,
       image: markerImage,
     });
+    setMarkers(prev => [...prev, newMarker]);
   });
 };
